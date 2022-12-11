113e1d8b6052325c0b3ed0f7931ca476a498cf04
Abir Hasan <cse.170201063@gmail.com>
2022-12-12 03:30:18 +0600

    footer responsive

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -4,37 +4,42 @@ import footer from '../../assets/images/footer.gif'
 const Footer = () => {
     return (
       <div className="relative mt-20">
-        <img className='w-full' src={footer} alt="" />
+        <img className="w-full" src={footer} alt="" />
 
         <div className="absolute bottom-0">
-          <div className="ml-9 mb-10 text-black">
+          <div className="lg:ml-9 lg:mb-10 ml-4 mb-3 text-black">
             <ul className="flex">
-              <li className="p-2">
+              <li className="lg:p-2">
                 <a
-                  className="text-4xl"
+                  className="lg:text-4xl"
                   href="https://www.linkedin.com/in/strikerabir/"
                 >
                   <FaLinkedin></FaLinkedin>
                 </a>
               </li>
-              <li className="p-2">
-                <a className="text-4xl" href="https://github.com/StrikerAbir">
+              <li className="lg:p-2">
+                <a
+                  className="lg:text-4xl"
+                  href="https://github.com/StrikerAbir"
+                >
                   <FaGithubSquare></FaGithubSquare>
                 </a>
               </li>
-              <li className="p-2">
+              <li className="lg:p-2">
                 <a
-                  className="text-4xl"
+                  className="lg:text-4xl"
                   href="https://www.facebook.com/strikerton.63"
                 >
                   <FaFacebookSquare></FaFacebookSquare>
                 </a>
               </li>
             </ul>
-          <div className="px-2 flex items-center justify-center">
-            <FaCopyright></FaCopyright>
-            <p className="ml-2">Copyright 2022, AbirHasan. </p>
-          </div>
+            <div className="lg:px-2 flex text-xs items-center justify-center">
+              <FaCopyright></FaCopyright>
+              <p className="ml-2">
+                Copyright <span className='lg:text-base-100 text-white'>2022, AbirHasan.</span>{" "}
+              </p>
+            </div>
           </div>
         </div>
       </div>
