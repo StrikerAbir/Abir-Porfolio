bd67acea94f646d4f610bab216bdd1171da91ce9
Abir Hasan <cse.170201063@gmail.com>
2022-12-12 03:35:40 +0600

    skills responsive

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -52,23 +52,23 @@ const Skills = () => {
     },
   ];
   return (
-    <div className="mt-10 h-screen" id="Skills">
+    <div className="mt-10 lg:h-screen" id="Skills">
       <div className="h-full flex justify-center items-center">
         <div className="bg-base-100 w-full py-20">
           <p className="text-3xl mb-3 font-bold text-primary text-center">
             MY SKILLS
           </p>
-                  <div className='grid grid-cols-3 gap-2'>
+                  <div className='grid lg:grid-cols-3 grid-cols-2 gap-2'>
                       {
                           data.map(skill => {
                               return (
                                 <>
                                   <div data-aos="zoom-in" className="px-5 my-4">
                                     <div className="flex justify-between">
-                                      <p className="text-xl font-bold">
+                                      <p className="lg:text-xl font-bold">
                                         {skill.name}
                                       </p>
-                                      <p className="text-xl font-bold text-warning">
+                                      <p className="lg:text-xl font-bold text-warning">
                                         {skill.percentage}%
                                       </p>
                                     </div>
